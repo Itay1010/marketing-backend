01c1417d9f1a7970e21bcc4047f795b20e56e020
Itay Zonshine <ezonsh@email.com>
2022-05-17 22:04:34 +0300

    moving to mongo atlas

---
+++ api/toy/toy.service.js
@@ -0,0 +1,78 @@
+const dbService = require('../../services/db.service')
+const logger = require('../../services/logger.service')
+const ObjectId = require('mongodb').ObjectId
+
+async function query(filterBy) {
+    try {
+        const criteria = _buildCriteria(filterBy)
+
+        const collection = await dbService.getCollection('toy')
+        var toys = await collection.find(criteria).toArray()
+        return toys
+    } catch (err) {
+        logger.error('cannot find toys', err)
+        throw err
+    }
+}
+
+async function getById(toyId) {
+    try {
+        const collection = await dbService.getCollection('toy')
+        const toy = collection.findOne({ _id: ObjectId(toyId) })
+        return toy
+    } catch (err) {
+        logger.error(`while finding toy ${toyId}`, err)
+        throw err
+    }
+}
+
+async function remove(toyId) {
+    try {
+        const collection = await dbService.getCollection('toy')
+        await collection.deleteOne({ _id: ObjectId(toyId) })
+        return toyId
+    } catch (err) {
+        logger.error(`cannot remove toy ${toyId}`, err)
+        throw err
+    }
+}
+
+async function add(toy) {
+    try {
+        const collection = await dbService.getCollection('toy')
+        const addedCar = await collection.insertOne(toy)
+        return addedCar
+    } catch (err) {
+        logger.error('cannot insert toy', err)
+        throw err
+    }
+}
+async function update(toy) {
+    try {
+        var id = ObjectId(toy._id)
+        delete toy._id
+        const collection = await dbService.getCollection('toy')
+        await collection.updateOne({ _id: id }, { $set: { ...toy } })
+        return toy
+    } catch (err) {
+        logger.error(`cannot update toy ${toyId}`, err)
+        throw err
+    }
+}
+
+function _buildCriteria({ txt, inStock, label }) {
+    var criteria = { $and: [] }
+    const reg = { $regex: txt, $options: 'i' }
+    if (txt) criteria.$and.push({ name: reg })
+    if (inStock !== '') criteria.$and.push({ inStock: JSON.parse(inStock) })
+    if(criteria.$and.length <= 1) criteria = {...criteria.$and['0']}
+    return criteria
+}
+
+module.exports = {
+    remove,
+    query,
+    getById,
+    add,
+    update,
+}
---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 'dbURL': 'mongodb+srv://Itay:DP4jyp7A6j6Sulj2@cluster0.agsr5.mongodb.net/?retryWrites=true&w=majority',
+  'dbURL': 'mongodb://localhost:27017',
+}
